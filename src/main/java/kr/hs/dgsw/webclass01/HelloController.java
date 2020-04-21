6174e1a36493bbcc965e2d4e4099532a29576b96
NameLoki <3334635@naver.com>
2020-04-21 15:13:42 +0900

    [과제]2020/04/21 계산기 만들기

--- src/main/java/kr/hs/dgsw/webclass01/HelloController.java
+++ src/main/java/kr/hs/dgsw/webclass01/HelloController.java
@@ -1,12 +1,30 @@
 package kr.hs.dgsw.webclass01;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping
-    public String hello() {
-        return "Hellow World!";
-    } 
-} 
+    // @GetMapping
+    // public String hello() {
+    //     return "Hellow World!";
+    // } 
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam final String say, @RequestParam final String to) {
+        return say + " " + to;
+    }
+
+    @GetMapping("/greeting1/{say}/{to}")
+    public String greeting1(@PathVariable final String say, @PathVariable final String to) { // Path형식
+        return say + " " + to + " greeting1";
+    }
+
+    @PostMapping("/greetingPost")
+    public String greetingPost(@RequestParam final String sayPost, @RequestParam final String toPost) {
+        return sayPost + " " + toPost + "Post";
+    }
+}
